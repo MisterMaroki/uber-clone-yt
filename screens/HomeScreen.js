2d0b17b4cae9d3a4d690610aa40c1bd66016677e
Omar Maroki <omar987@hotmail.co.uk>
2022-06-26 15:59:49 +0100

    Bring in Google places autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { View, Text, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const HomeScreen = () => {
 	return (
@@ -11,6 +13,28 @@ const HomeScreen = () => {
 					style={{ width: 100, height: 100, resizeMode: 'contain' }}
 					source={{
 						uri: 'https://links.papareact.com/gzs',
+					}}
+				/>
+				<GooglePlacesAutocomplete
+					nearbyPlacesAPI="GooglePlacesSearch"
+					styles={{
+						container: {
+							flex: 0,
+						},
+						textInput: {
+							fontSize: 18,
+						},
+					}}
+					enablePoweredByContainer={false}
+					minLength={2}
+					returnKeyType="search"
+					onPress={(data, details = null) => {
+						console.log(data, details)
+					}}
+					placeholder="Where from?"
+					query={{
+						key: GOOGLE_MAPS_APIKEY,
+						language: 'en',
 					}}
 				/>
 				<NavOptions />
